f7fe00d940e5a3b6dd069aef77b52563a2c77e53
angusking47 <angusking47@link.cuhk.edu.hk>
2022-07-04 22:35:10 +0800

    Data selfie app finished (need polish)

--- Module 2/Exercise_2/index.js
+++ Module 2/Exercise_2/index.js
@@ -1,22 +1,35 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
-//short version of database
-const db = [];
-
+//nedb database
+const database = new Datastore("Database.db");
+database.loadDatabase();
 app.listen(3000 , () => console.log('Listening at 3000'));
 app.use(express.static("public"));
 app.use(express.json({limit : "1mb"}));
+app.get('/api' , (request, response) => {
+    database.find({} , (err, data) => {
+        response.json(data);
+    });
+    }
+    
+)
 app.post('/api' , (request, response) => { 
     const lon = request.body.lon;
     const lat = request.body.lat;
+    const food = request.body.food;
     console.log('I got a request');
-    console.log(request.body);
-    db.push({lon , lat});
+    const timestamp = Date.now();
+    const image64 = request.body.image64
+    data = {lat , lon , food , image64 , timestamp};
+    database.insert(data);
     response.json({
         status: "sucess",
-        lon: request.body.lon,
-        lat: request.body.lat
+        lon: lon,
+        lat: lat,
+        food: food,
+        image64: image64
     });
-    console.log("Below is all ther request")
-    console.log(db);
+    //console.log("Below is all ther request")
+    //console.log(db);
 })
