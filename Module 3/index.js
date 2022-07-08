c0f24b1a7a675bbcba6aaf3a4563a8b2aef14587
angusking47 <angusking47@link.cuhk.edu.hk>
2022-07-08 18:51:58 +0800

    Finished Weather here

--- Module 3/index.js
+++ Module 3/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const Datastore = require('nedb');
 const fetch = require('node-fetch');
 const app = express();
+require('dotenv').config();
+
 //nedb database
 const database = new Datastore("Database.db");
 database.loadDatabase();
 app.listen(3000 , () => console.log('Listening at 3000'));
 app.use(express.static("public"));
 app.use(express.json({limit : "1mb"}));
+
 app.get('/api' , (request, response) => {
     database.find({} , (err, data) => {
         response.json(data);
@@ -17,35 +20,44 @@ app.get('/api' , (request, response) => {
 )
 
 app.get("/weather/:latlon" , async (request, response) => {
+    const api_key = process.env.API_KEY;
     const latlon = request.params.latlon.split(",");
     const lat = latlon[0];
     const lon = latlon[1];
-    const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=14f69856f32ac175e31c3ed272968b95`
-    console.log(api_url);
-    const api_response = await fetch(api_url);
-    const json = await api_response.json();
-    response.json(json);
+    const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`
+    const aq_url = `https://api.openaq.org/v2/latest?coordinates=${lat}%2C${lon}`
+    const weather_response = await fetch(api_url);
+    const weather_data = await weather_response.json();
+    const aq_response = await fetch(aq_url);
+    const aq_data = await aq_response.json();
+    const data = {
+        weather:weather_data,
+        air_quality:aq_data
+    }
+    response.json(data);
     
 })
 //https://api.openweathermap.org/data/2.5/weather?lat=22.34&lon=114.16°&appid=14f69856f32ac175e31c3ed272968b95
 
 
 app.post('/api' , (request, response) => { 
-    const lon = request.body.lon;
     const lat = request.body.lat;
-    const food = request.body.food;
-    console.log('I got a request');
+    const lon = request.body.lon;
+    const weather_summary = request.body.weather_summary;
+    const weather_station = request.body.weather_station;
+    const weather_temperature = request.body.weather_temperature;
+    const aq = request.body.aq;
     const timestamp = Date.now();
-    const image64 = request.body.image64
-    data = {lat , lon , food , image64 , timestamp};
+    console.log('I got a request');
+    data = {lat , lon , weather_summary , weather_station , weather_temperature , aq , timestamp};
     database.insert(data);
     response.json({
         status: "sucess",
-        lon: lon,
-        lat: lat,
-        food: food,
-        image64: image64
+        lat:lat,
+        lon:lon,
+        weather_summary: weather_summary,
+        weather_station: weather_station,
+        weather_temperature: weather_temperature,
+        aq: aq
     });
-    //console.log("Below is all ther request")
-    //console.log(db);
 })
